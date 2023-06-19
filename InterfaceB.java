0d8e796c53fa47e2a42a7089283ddf589aeacf1f
Totenkopf1995 <121690654+Totenkopf1995@users.noreply.github.com>
2023-06-18 21:49:56 -0500

    Delete InterfaceB.java

--- InterfaceB.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface InterfaceB {
-    public abstract void mostrarMensaje();
-
-    public default void saluda(){
-        System.out.println("Hola mundo, desde una interface!");
-    }
-}
